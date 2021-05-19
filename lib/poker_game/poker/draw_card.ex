1675c76bde19d67ae4d118a2cadea16db945b1ca
WanjikuMac <edwardscaroline32@gmail.com>
2021-05-19 13:35:41 +0300

    apply formatting'

--- lib/poker_game/poker/draw_card.ex
+++ lib/poker_game/poker/draw_card.ex
@@ -1,44 +1,45 @@
-defmodule PokerGame.DrawCard do 
-    require Logger
-    @cards ~w(2 3 4 5 6 7 8 9 T J Q K A)
-    #define the data structure
-    defstruct [:black, :white]
+defmodule PokerGame.DrawCard do
+  require Logger
+  @cards ~w(2 3 4 5 6 7 8 9 T J Q K A)
+  # define the data structure
+  defstruct [:black, :white]
 
-    #create constructor
-    #A hand only contains 5 cards
-    def new(value_a, value_b) do
-     %__MODULE__{black: value_a, white: value_b}
-    end
+  # create constructor
+  # A hand only contains 5 cards
+  def new(cards_a, cards_b) do
+    %__MODULE__{black: cards_a, white: cards_b}
+  end
 
-    #create reducer 
-    def high_card(%{black: value_a, white: value_b}) do
-        
-        # high_card(value_a) |> IO.inspect
-        # high_card(value_b) |> IO.inspect
-     {player, card, _index} = Enum.max_by([high_card({value_a, "black"}), high_card({value_b, "white"})], fn {_player, _card, index} -> index end) 
-     
-     "#{player} wins - high card: #{name(card)}"
-        # Logger.warn("the number of cards drawn are not 5")
-    end
+  # create convertor
+  def high_card(%{black: cards_a, white: cards_b}) do
+    {player, card, _index} =
+      Enum.max_by([high_card({cards_a, "black"}), high_card({cards_b, "white"})], fn {_player,
+                                                                                      _card,
+                                                                                      index} ->
+        index
+      end)
 
-    def high_card({value, player}) when is_list(value) do 
-       result = value
-                |> Enum.map(fn x -> String.split_at(x, 1) end)
-                |> Enum.map(fn x -> elem(x, 0) end)
-    
-        {card, index} = Enum.filter(ranked_cards(), fn ({x, _y}) -> Enum.find(result, fn (x1) -> x1 == x end) end) |> Enum.max() 
-        {player, card, index}
-    end
+    "#{player} wins - high card: #{name(card)}"
+  end
 
-    defp ranked_cards() do
-       Enum.zip(@cards, 1..13)
-    end
+  # create reducer 
+  def high_card({cards, player}) when is_list(cards) do
+    result =
+      cards
+      |> Enum.map(fn x -> String.split_at(x, 1) end)
+      |> Enum.map(fn x -> elem(x, 0) end)
 
-    defp name("J"), do: "Jack"
-    defp name("Q"), do: "Queen"
-    defp name("K"), do: "King"
-    defp name("A"), do: "Ace"
-    defp name(value), do: value 
-    
-    
+    {card, index} = Enum.filter(ranked_cards(), fn {card, _y} -> card in result end) |> Enum.max()
+    {player, card, index}
+  end
+
+  defp ranked_cards() do
+    Enum.zip(@cards, 1..13)
+  end
+
+  defp name("J"), do: "Jack"
+  defp name("Q"), do: "Queen"
+  defp name("K"), do: "King"
+  defp name("A"), do: "Ace"
+  defp name(value), do: value
 end
