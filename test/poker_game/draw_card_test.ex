fa6dec42bf3a43902a7fb6ede2264522837fd00f
WanjikuMac <edwardscaroline32@gmail.com>
2021-05-19 14:27:36 +0300

    add explict indication of which player won

--- test/poker_game/draw_card_test.ex
+++ test/poker_game/draw_card_test.ex
@@ -2,8 +2,10 @@ defmodule PokerGame.DrawCardTest do
   use ExUnit.Case
   import PokerGame.DrawCard
 
-  test "card scores with high card criteria" do
-    result  = new(~w(2H 3D 5S 9C KD), ~w(2C 3H 4S 8C AH))
+  test "high_card/1" do
+    white_player = ~w(2C 3H 4S 8C AH)
+    black_player = ~w(2H 3D 5S 9C KD)
+    result  = new(black, white)
               |> high_card()
     assert result == "white wins - high card: Ace"
   end
